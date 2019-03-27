c038b8176fca32a3cdc181f314d6a1180eff962f
Long Phu <phu.huynh@monimedia.com>
2019-03-27 18:06:44 +0700

    Add Go Dep & Main File

---
+++ main.go
@@ -0,0 +1,25 @@
+// Our first program
+package main
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	fmt.Println("======================================")
+
+	log.WithFields(log.Fields{
+		"fields": "values",
+	}).Info("Log Info With Fields")
+	log.Trace("Something very low level.")
+	log.Debug("Useful debugging information.")
+	log.Info("Something noteworthy happened!")
+	log.Warn("You should probably take a look at this.")
+	log.Error("Something failed but I'm not quitting.")
+	// Calls os.Exit(1) after logging
+	log.Fatal("Bye.")
+	// Calls panic() after logging
+	log.Panic("I'm bailing.")
+}
