f0653b408bc82a07e0e0625876155f8e833c290f
Long Phu <phu.huynh@monimedia.com>
2019-03-28 17:21:12 +0700

    Add file tut

---
+++ tut_test.go
@@ -0,0 +1,54 @@
+// https://tutorialedge.net/golang/improving-your-tests-with-testify-go/
+// https://tutorialedge.net/golang/benchmarking-your-go-programs/
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculate(t *testing.T) {
+	if Calculate(2) != 4 {
+		t.Error("Expected 2 + 2 to equal 4")
+	}
+}
+
+func TestCalculateWithAssert(t *testing.T) {
+	assert.Equal(t, Calculate(2), 4)
+}
+
+func TestCalculateTableDriven(t *testing.T) {
+	assert := assert.New(t)
+
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{2, 4},
+		{-1, 1},
+		{0, 2},
+		{-5, -3},
+		{99999, 100001},
+	}
+
+	for _, test := range tests {
+		assert.Equal(Calculate(test.input), test.expected)
+	}
+}
+
+func BenchmarkCalculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Calculate(2)
+	}
+}
+
+func benchmarkCalculate(input int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Calculate(input)
+	}
+}
+
+func BenchmarkCalculate100(b *testing.B)         { benchmarkCalculate(100, b) }
+func BenchmarkCalculateNegative100(b *testing.B) { benchmarkCalculate(-100, b) }
+func BenchmarkCalculateNegative1(b *testing.B)   { benchmarkCalculate(-1, b) }
